54a2e90b66ae8937ce39f0b53339dfda82b04f1c
Alex Kempton <alex@funwithtriangles.net>
2021-02-04 14:35:47 +0100

    Fixed issue with aspect ratio correction

--- src/utils/getResizeFactors.ts
+++ src/utils/getResizeFactors.ts
@@ -7,12 +7,12 @@ export const getResizeFactors = (
   let x;
   let y;
 
-  if (sWidth > sHeight) {
-    // Landscape
+  if (sWidth / sHeight > dWidth / dHeight) {
+    // Source has wider aspect ratio than destination
     x = (dWidth / sWidth) * (sHeight / dHeight);
     y = 1;
   } else {
-    // Portrait
+    // Source has narrower aspect ratio than destination
     x = 1;
     y = (sWidth / dWidth) * (dHeight / sHeight);
   }
