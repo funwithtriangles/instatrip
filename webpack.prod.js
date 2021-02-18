784b63ba933c209877468225720f9df03956c3e7
Alex Kempton <alex@funwithtriangles.net>
2021-02-18 17:33:13 +0100

    Updated public path for prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,13 @@
 // const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const commonConfig = require('./webpack.common');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(commonConfig, {
   mode: 'production',
   output: {
     path: `${__dirname}/dist`,
-    publicPath: '/',
+    publicPath: '/instatrip',
     filename: 'app.min.js',
   },
   // Useful for analysing bundle, keep commented out otherwise (breaks netlify build)
